edc50c9b2979214188271e8c7e19912e3d3081d7
Rahul Lodhi <111701793+RahulLodhi12@users.noreply.github.com>
2025-04-19 21:51:37 +0530

    Time: 1 ms (100%), Space: 46.4 MB (12.18%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,37 +3,47 @@ class Solution {
         int n=nums1.length;
         int m=nums2.length;
 
-        int[] temp = new int[n+m];
+        int idx1=(n+m)/2;
+        int idx2=idx1-1;
 
+        int k=0; //index
         int i=0,j=0;
-        int k=0;
+
+        int el1=-1,el2=-1;//answers
 
         while(i<n && j<m){
             if(nums1[i]<=nums2[j]){
-                temp[k++]=nums1[i];
+                if(k==idx1) el1 = nums1[i];
+                if(k==idx2) el2 = nums1[i];
                 i++;
+                k++;
             }
             else{
-                temp[k++]=nums2[j];
+                if(k==idx1) el1 = nums2[j];
+                if(k==idx2) el2 = nums2[j];
                 j++;
+                k++;
             }
         }
 
         while(i<n){
-            temp[k++]=nums1[i++];
+            if(k==idx1) el1 = nums1[i];
+            if(k==idx2) el2 = nums1[i];
+            i++;
+            k++;
         }
 
         while(j<m){
-            temp[k++]=nums2[j++];
+            if(k==idx1) el1 = nums2[j];
+            if(k==idx2) el2 = nums2[j];
+            j++;
+            k++;
         }
 
-        int len = temp.length;
+        // System.out.println(el1+" "+el2);
 
-        if(len%2==0){
-            double median = (double)(temp[len/2] + temp[(len/2)-1])/2;
-            return median;
-        }
+        if((n+m)%2==0) return (double)(el1+el2)/2;
 
-        return (double)temp[len/2];
+        return el1;
     }
 }
