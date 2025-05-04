302815324de998a273496186a7a6b5e05fbbda31
Rahul Lodhi <111701793+RahulLodhi12@users.noreply.github.com>
2025-05-04 12:40:40 +0530

    Time: 11 ms (2.85%), Space: 41.6 MB (7.56%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,18 +1,25 @@
 class Solution {
     public int maxProduct(int n) {
-        //approach 1: O(n) space, O(nlogn) time
-        ArrayList<Integer> lst = new ArrayList<>();
-        
+        //approach 2: O(n) time
+        int max1=Integer.MIN_VALUE; //largest
+        int max2=Integer.MIN_VALUE; //second largest
+
         while(n!=0){
             int d = n%10;
-            lst.add(d);
+
+            if(d>=max1){
+                max2 = max1;
+                max1 = d;
+            }
+            else if(d>max2 && d!=max1){
+                max2 = d;
+            }
+
             n=n/10;
         }
-        
-        Collections.sort(lst);
-        
-        int len = lst.size();
-        
-        return lst.get(len-1)*lst.get(len-2);
+
+        System.out.println(max1+" "+max2);
+
+        return max1*max2;
     }
 }
