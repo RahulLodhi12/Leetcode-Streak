973f9d08500d54dc15ecca62213d1d2618e24520
Rahul Lodhi <111701793+RahulLodhi12@users.noreply.github.com>
2025-05-10 14:12:14 +0530

    Time: 0 ms (100%), Space: 43.6 MB (10.63%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,26 +1,16 @@
 class Solution {
+    public double solve(double x, long n){
+        if(n==0) return 1;
+
+        if(n<0) return solve(1/x,-n);
+
+        if(n%2==0){
+            return solve(x*x,n/2); //even
+        }
+
+        return x*solve(x,n-1); //odd -> ans store here
+    }
     public double myPow(double x, int n) {
-        double ans=1;
-
-        long p=n;
-        long orgPow=n;
-        if(orgPow<0){
-            p=-p;
-        }
-
-        while(p>0){ //reducing power
-            if(p%2==0){ //even
-                x=x*x;
-                p=p/2;
-            }
-            else{ //odd
-                ans=ans*x;
-                p--;
-            }
-        }
-
-        if(orgPow<0) return 1/ans;
-
-        return ans;
+        return solve(x,(long)n);
     }
 }
