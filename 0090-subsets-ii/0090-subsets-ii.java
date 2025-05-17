4aa29430aa37fb5d1c599cc21dbd3058dedb837d
Rahul Lodhi <111701793+RahulLodhi12@users.noreply.github.com>
2025-05-17 09:23:22 +0530

    Time: 1 ms (99.92%), Space: 43.6 MB (60.95%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,28 +1,27 @@
 class Solution {
-    public static void solve(int[] arr, List<Integer> temp, int idx, int n, Set<List<Integer>> ans){
+    public void printAllUniqueSubsets_optimal(int[] arr, int i, int n, List<Integer> temp, List<List<Integer>> ans){
+        ans.add(new ArrayList<>(temp)); //add every current/temp subset
+
         //base case
-        if(idx==n){
-            ans.add(new ArrayList<>(temp));
-            return;
+        if(i==n) return;
+
+        for(int k=i;k<n;k++){
+            if(k>i && arr[k]==arr[k-1]) continue;
+
+            temp.add(arr[k]);
+            printAllUniqueSubsets_optimal(arr,k+1,n,temp,ans);
+            temp.remove(temp.size()-1); //backtrack
         }
-
-        //take
-        temp.add(arr[idx]);
-        solve(arr,temp,idx+1,n,ans);
-
-        //skip
-        temp.remove(temp.size()-1);
-        solve(arr,temp,idx+1,n,ans);
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         int n=nums.length;
-        Arrays.sort(nums);
+        Arrays.sort(nums); //sort helps us to deal with duplicates easily.
 
         List<Integer> temp = new ArrayList<>();
-        Set<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
 
-        solve(nums,temp,0,n,ans);
+        printAllUniqueSubsets_optimal(nums,0,n,temp,ans);
         
-        return new ArrayList<>(ans);
+        return ans;
     }
 }
