e8567ceede8817d07a20b041b3bf097a6eaae493
Rahul Lodhi <111701793+RahulLodhi12@users.noreply.github.com>
2025-07-07 14:29:43 +0000

    Time: 52 ms (100%), Space: 45.7 MB (100%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.java
+++ 3606-coupon-code-validator/3606-coupon-code-validator.java
@@ -1,6 +1,6 @@
 class Pair<U, V> {
-    public final U first;
-    public final V second;
+    public U first;
+    public V second;
 
     public Pair(U first, V second) {
         this.first = first;
