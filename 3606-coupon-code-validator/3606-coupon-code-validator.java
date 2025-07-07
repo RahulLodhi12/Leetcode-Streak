16fc3fa851d3fc6aaca069ac9659e01a7e6fdf9b
Rahul Lodhi <111701793+RahulLodhi12@users.noreply.github.com>
2025-07-07 14:25:33 +0000

    Time: 49 ms (100%), Space: 46 MB (100%) - LeetHub

---
+++ 3606-coupon-code-validator/3606-coupon-code-validator.java
@@ -0,0 +1,46 @@
+class Pair<U, V> {
+    public final U first;
+    public final V second;
+
+    public Pair(U first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+class Solution {
+    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
+        List<String> ans = new ArrayList<>();
+        
+        List<Pair<String,String>> lst = new ArrayList<>();
+
+        int n=code.length;
+
+        for(int i=0;i<n;i++){
+            String c = code[i];
+            String bl = businessLine[i];
+            if(c.matches("[a-zA-Z0-9_]+") && isActive[i] && !bl.equals("invalid")){
+                lst.add(new Pair(c,bl));
+            }
+        }
+
+        System.out.println(lst.size());
+        for(Pair<String,String> p: lst){
+            System.out.println(p.first + " " + p.second);
+        }
+
+        lst.sort((a,b)->{
+            if(a.second.equals(b.second)){
+                return a.first.compareTo(b.first);
+            }
+            return a.second.compareTo(b.second);
+        });
+
+        for(Pair<String,String> p: lst){
+            // System.out.println(p.first + " " + p.second);
+            ans.add(p.first);
+        }
+
+        return ans;
+    }
+}
